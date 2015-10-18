f1fd87677d09f8187a2eb44f72f3683390aa7209
LeMasque <masquarainian@live.com>
2015-10-18 10:27:18 +0000

    somewhat workable serverLog changes

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ http.listen(8080, function(){
 	console.log('program listening on *:8080');
 });
 
+/********************************** ROUTING **********************************/
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/view/index.html");
 });
 
-
+/****************************** EVENT HANDLERS *******************************/
 io.on('connection', function(socket){
 	console.log('user connected');
 	socket.on("chat message", function(content) {
@@ -53,5 +54,27 @@ function permitAccessTo(path) {
 
 function serverLog(type, message, extra) {
 	if (typeof type !== "number") {
-		serverLog(-1, "serverLog type 
+		badType("serverLog", type, "number");
+	}
+	if (typeof message !== "string") {
+		badType("serverLog", message, "string");
+	}
+	if (type > 0) {
+
+	} else if (type === 0) {
+
+	} else if (type < 0) {
+		if (type === -1) {
+			chalk.red(message);
+		} else if (type === -2) {
+			chalk.bgRed(message);
+		}
+	}
+
+}
+
+function badType(fName, thing, expected) {
+	var blah = fName + " type mismatch; a " + typeof thing + " was provided ";
+	blah += "-- expected a " + expected;
+	serverLog(-1, blah);
 }
