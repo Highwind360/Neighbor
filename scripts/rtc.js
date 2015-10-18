99dcdcfe7f4ce8265a92067c0cfcb8bd5228bff2
highwind <blank@blank.com>
2015-10-18 17:06:27 +0000

    added necessary script for rtc

---
+++ scripts/rtc.js
@@ -0,0 +1,38 @@
+// construct webrtc and point it at the divs
+var webrtc = new SimpleWebRTC({
+	localVideoEl: 'localVideo',
+	remoteVideoEl: 'remoteVideo',
+	autoRequestMedia: true,
+});
+
+// note: 'coolroom' is the room being joined
+webrtc.on('readyToCall', function() {
+	webrtc.joinRoom('coolroom');
+});
+
+// whenever a user joins the room, adds them to remoteVideo div
+webrtc.on('videoAdded', function (video, peer) {
+	var remotes = document.getElementById('remoteVideo');
+	if(remotes) {
+		var container = document.createElement('div');
+		container.className = 'videoContainer';
+		container.id = 'container_' + webrtc.getDomId(peer);
+		container.appendChild(video);
+		remotes.appendChild(container);
+	}
+});
+
+// whenever a user leaves the room, removes them from remoteVideo div and
+// displays a leave message
+webrtc.on('videoRemoved', function (video, peer) {
+	var remotes = document.getElementById('remoteVideo');
+	var el = document.getElementById(peer ? 'container_' + webrtc.getDomId(peer) : 
+		'localScreenContainer');
+	if(remotes && el) {
+		el.removeChild(video);
+		var mesg = document.createElement('p');
+		mesg.classList.add("disconnect-mesg");
+		mesg.textContent = "User disconnected";
+		el.appendChild(mesg);
+	}
+});
